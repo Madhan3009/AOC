95dfec085b7f57ba5910b325c552d68eb8f37b01
Madhan <madhankumaar96@gmail.com>
2025-12-27 17:41:00 +0530

    Initial commit

---
+++ utils/fileReader.go
@@ -0,0 +1,59 @@
+package utils
+
+
+import(
+	"fmt"
+	"os"
+	"log"
+	"strconv"
+	"strings"
+	"path/filepath"
+)
+func Read(fileName string)([]int,[]int,error){
+	path := buildPath(fileName)
+	input1,input2,err := readShit(path)
+	return input1,input2,err
+}
+func buildPath(fileName string) string{
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Error getting current working directory: %v", err)
+	}
+
+	fmt.Println("Current working directory:", cwd)
+
+	
+	fullPath := filepath.Join(cwd, fileName)
+
+	return fullPath
+}
+func readShit(path string)([]int,[]int,error){
+	data,err := os.ReadFile(path)
+	if(err!=nil){
+		log.Fatal(err)
+	}
+
+
+	
+	lines := strings.Split(strings.TrimSpace(string(data)),"\n")
+	left := make([]int,0,len(lines))
+	right := make([]int,0,len(lines))
+	
+	for _,line := range lines{
+		fields := strings.Fields(line)
+		
+		lString := fields[0]
+		rString := fields[1]
+		
+		val1,_ := strconv.Atoi(lString)
+		left = append(left,val1)
+
+		val2,_ := strconv.Atoi(rString)
+		right = append(right,val2)
+	
+	}
+
+	return left,right,nil
+}
+
+
